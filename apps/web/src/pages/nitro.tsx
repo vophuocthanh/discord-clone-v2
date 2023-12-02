a1791fa5f38604277f5e22b5f16a0e5dce18ff58
Võ Phước Thạnh <phuocthanh2k03@gmail.com>
2023-12-02 20:52:27 +0700

    feat: UI

--- apps/web/src/pages/nitro.tsx
+++ apps/web/src/pages/nitro.tsx
@@ -1,18 +1,30 @@
 import { Link } from 'react-router-dom';
 import logo from '@/assets/images/logo.png';
 import { Button } from '@/components/ui/button';
-import video from '@/assets/images/video.webm';
-import ios from '@/assets/images/ios.svg';
-import linux from '@/assets/images/linux.svg';
-import android from '@/assets/images/android.svg';
-import mac from '@/assets/images/mac.svg';
-import { DropdownButton } from './_components/dropdown/DropdownButton';
-import { DropdownDownloadPublic } from './_components/dropdown/DropdownDownloadPublic';
+import nitroVideo from '@/assets/images/video.webm';
+import biggeer from '@/assets/images/bigger.svg';
+import swet from '@/assets/images/swet.svg';
+import emoji from '@/assets/images/emoji.svg';
+import unlock from '@/assets/images/unlock.svg';
 import Footer from './_components/Footer';
+import {
+  ArrowUpFromLine,
+  BadgePercent,
+  PersonStanding,
+  ScreenShare,
+  Server,
+  Smile,
+  SmilePlus,
+} from 'lucide-react';
 export default function Component() {
   return (
-    <div className=' w-full h-full'>
-      <header className='mx-auto bg-[#23272a] justify-center flex'>
+    <div className='relative w-full'>
+      <div className='h-[20rem]'>
+        <video width='100%' height='100%' autoPlay loop muted>
+          <source src={nitroVideo} type='video/webm' />
+        </video>
+      </div>
+      <header className='absolute left-0 right-0 top-0 mx-auto justify-center flex'>
         <div className='h-[40rem] max-w-6xl mt-12 text-white flex flex-col items-center'>
           <div className='flex items-center justify-between w-full gap-20'>
             <Link to='/'>
@@ -47,71 +59,112 @@ export default function Component() {
               </Button>
             </Link>
           </div>
-          {/* <SectionInView className='flex justify-between max-w-6xl gap-10 mt-20'>
-            <div className=''>
-              <h1 className='text-6xl font-bold'>
-                GET DISCORD <br /> FOR ANY <br /> DEVICE
-              </h1>
-              <p className='mt-10'>
-                An adventure awaits. Hang out with your friends on our desktop
-                app <br /> and keep the conversation going on mobile.
-              </p>
-              <div className='mt-10 flex gap-10 items-center'>
-                <Button className='flex h-16 gap-4 px-6 text-xl text-black bg-white rounded-full hover:bg-white hover:text-primary hover:shadow-xl'>
-                  <span>
-                    <Download />
+          <div className='w-96 mb-[18rem] mr-auto px-10 mt-20'>
+            <h1 className='text-7xl text-white font-bold'>
+              Unleash more fun with Nitro
+            </h1>
+            <p>
+              Subscribe to Nitro to upgrade your emoji, personalize your
+              profile, share bigger files, and so much more.{' '}
+            </p>
+          </div>
+          <div className='flex justify-between gap-10 mx-auto max-w-6xl'>
+            <div className='w-[36rem] flex justify-between flex-col p-6 h-[31rem] bg-white bg-gradient-to-br from-blue-500 via-blue-600 to-indigo-500 rounded'>
+              <div className='flex flex-col space-y-4'>
+                <h1 className='text-4xl uppercase font-bold'>Nitro Basic</h1>
+                <div className='flex gap-6 items-center'>
+                  <ArrowUpFromLine />
+                  <span className='text-xl'>50MB uploads</span>
+                </div>
+                <div className='flex gap-6 items-center'>
+                  <Smile />
+                  <span className='text-xl'>Custom emoji anywhere</span>
+                </div>
+                <div className='flex gap-6 items-center'>
+                  <SmilePlus />
+                  <span className='text-xl'>Unlimited Super Reactions</span>
+                </div>
+                <div className='flex gap-6 items-center'>
+                  <BadgePercent />
+                  <span className='text-xl'>
+                    Special Nitro badge on your profile
                   </span>
-                  Download for Windows
-                </Button>
-                <span>Windows 7 or higher</span>
+                </div>
               </div>
+              <Button className='w-full h-16 rounded-full bg-white text-black hover:bg-white hover:shadow-xl text-xl hover:text-primary'>
+                Subscribe
+              </Button>
             </div>
-          </SectionInView> */}
+            <div className='w-[36rem] p-6 space-y-10 h-[31rem] bg-white rounded bg-gradient-to-tr flex flex-col justify-between from-purple-600 via-pink-600 to-rose-500'>
+              <div className='flex flex-col space-y-4'>
+                <h1 className='text-4xl uppercase font-bold'>Nitro</h1>
+                <div className='flex gap-6 items-center'>
+                  <ArrowUpFromLine />
+                  <span className='text-xl'>500MB uploads</span>
+                </div>
+                <div className='flex gap-6 items-center'>
+                  <Smile />
+                  <span className='text-xl'>Custom emoji anywhere</span>
+                </div>
+                <div className='flex gap-6 items-center'>
+                  <SmilePlus />
+                  <span className='text-xl'>Unlimited Super Reactions</span>
+                </div>
+                <div className='flex gap-6 items-center'>
+                  <ScreenShare />
+                  <span className='text-xl'>HD video streaming</span>
+                </div>
+                <div className='flex gap-6 items-center'>
+                  <Server />
+                  <span className='text-xl'>2 Server Boosts</span>
+                </div>
+                <div className='flex gap-6 items-center'>
+                  <PersonStanding />
+                  <span className='text-xl'>Custom profiles and more!</span>
+                </div>
+              </div>
+              <Button className='w-full h-16 rounded-full bg-white text-black hover:bg-white hover:shadow-xl text-xl hover:text-primary'>
+                Subscribe
+              </Button>
+            </div>
+          </div>
         </div>
       </header>
-      <video width='100%' height='360' autoPlay loop>
-        <source src={video} type='video/webm' />
-      </video>
-      <div className='flex text-black justify-between items-center max-w-6xl mx-auto gap-10 my-20'>
+      <div className='flex mt-[70rem] text-black justify-between items-center max-w-7xl mx-auto gap-10 my-20'>
         <div className='flex flex-col space-y-10'>
-          <div className='relative flex w-[36rem] overflow-hidden flex-col items-center space-y-6  rounded-lg h-[46rem] bg-[#f6f6f6]'>
-            <h1 className='text-4xl mt-28 font-bold'>iOS</h1>
-            <Button className='bg-black text-white rounded-full text-xl p-8 hover:bg-black hover:shadow-md hover:scale-110'>
-              Download
-            </Button>
-            <img src={ios} alt='ios' className='absolute top-[21.5rem]' />
+          <div className='flex w-[40rem] space-y-10 justify-center overflow-hidden flex-col items-center  rounded-lg h-[24rem] bg-[#f6f6f6]'>
+            <h1 className='text-4xl font-bold text-center'>
+              From clips to pics, share away with bigger file uploads
+            </h1>
+            <img src={biggeer} alt='biggeer' className=' top-[21.5rem]' />
           </div>
-          <div className='relative flex w-[36rem] flex-col overflow-hidden items-center space-y-6  rounded-lg h-[40rem] bg-[#f6f6f6]'>
-            <h1 className='text-4xl mt-28 font-bold'>Linux</h1>
-            <DropdownButton></DropdownButton>
-            <img src={linux} alt='ios' className='absolute top-[16rem]' />
-          </div>
-          <div className='relative flex w-[36rem] flex-col overflow-hidden items-center space-y-6 rounded-lg h-96 bg-[#f6f6f6]'>
-            <h1 className='text-4xl mt-28 font-bold'>Feeling experimental?</h1>
-            <p>
-              Try our Public Test Build and test new features before they
-              launch.
-            </p>
-            <DropdownDownloadPublic></DropdownDownloadPublic>
+          <div className='flex w-[40rem] space-y-10 justify-center overflow-hidden flex-col items-center  rounded-lg h-[24rem] bg-[#f6f6f6]'>
+            <h1 className='text-4xl font-bold text-center'>
+              Hype and meme with custom emoji anywhere
+            </h1>
+            <img src={emoji} alt='emoji' className=' top-[21.5rem]' />
           </div>
         </div>
         <div className='flex flex-col space-y-10'>
-          <div className='relative flex w-[36rem] overflow-hidden flex-col items-center space-y-6  rounded-lg h-[46rem] bg-[#f6f6f6]'>
-            <h1 className='text-4xl mt-14 font-bold'>Android</h1>
-            <Button className='bg-black text-white rounded-full text-xl p-8 hover:bg-black hover:shadow-md hover:scale-110'>
-              Download
-            </Button>
-            <img src={android} alt='ios' className='absolute top-[13.5rem]' />
-          </div>
-          <div className='relative flex w-[36rem] overflow-hidden flex-col items-center space-y-6  rounded-lg h-[38rem] bg-[#f6f6f6]'>
-            <h1 className='text-4xl mt-14 font-bold'>Mac</h1>
-            <Button className='bg-black text-white rounded-full text-xl p-8 hover:bg-black hover:shadow-md hover:scale-110'>
-              Download
-            </Button>
-            <img src={mac} alt='ios' className='absolute top-[13.5rem]' />
+          <div className='flex flex-col space-y-10'>
+            <div className='flex w-[40rem] space-y-10 justify-center overflow-hidden flex-col items-center  rounded-lg h-[24rem] bg-[#f6f6f6]'>
+              <h1 className='text-4xl font-bold text-center'>
+                Stream apps and games in sweet, sweet HD
+              </h1>
+              <img src={swet} alt='swet' className=' top-[21.5rem]' />
+            </div>
+            <div className='flex w-[40rem] space-y-10 justify-center overflow-hidden flex-col items-center  rounded-lg h-[24rem] bg-[#f6f6f6]'>
+              <h1 className='text-4xl font-bold text-center'>
+                Unlock perks for your communities with 2 Server Boosts
+              </h1>
+              <img src={unlock} alt='unlock' className=' top-[21.5rem]' />
+            </div>
           </div>
         </div>
       </div>
+      <Button className='mx-auto flex bg-black w-56 rounded-full my-20 hover:bg-black hover:shadow-md h-16 text-xl'>
+        Show all perks
+      </Button>
       <Footer />
     </div>
   );
--- apps/web/src/pages/safety.tsx
+++ apps/web/src/pages/safety.tsx
@@ -1,6 +1,117 @@
-
+import { Link } from 'react-router-dom';
+import logo from '@/assets/images/logo.png';
+import { Button } from '@/components/ui/button';
+import bannerSafety from '@/assets/images/bg-safety.svg';
+import ios from '@/assets/images/ios.svg';
+import linux from '@/assets/images/linux.svg';
+import android from '@/assets/images/android.svg';
+import mac from '@/assets/images/mac.svg';
+import { DropdownButton } from './_components/dropdown/DropdownButton';
+import { DropdownDownloadPublic } from './_components/dropdown/DropdownDownloadPublic';
+import Footer from './_components/Footer';
+import SectionInView from '@/components/SectionInView';
 export default function Component() {
   return (
-    <div>Safety</div>
-  )
+    <div className=' w-full h-full'>
+      <header className='mx-auto bg-[#5865f2] justify-center flex'>
+        <div className='h-[calc(100vh-4.5rem)] max-w-6xl mt-12 text-white flex flex-col items-center'>
+          <div className='flex items-center justify-between w-full gap-20'>
+            <Link to='/'>
+              <img src={logo} alt='logo' />
+            </Link>
+            <div className='flex items-center gap-10 text-lg font-medium'>
+              <Link to='/download' className='hover:underline'>
+                Download
+              </Link>
+              <Link to='/nitro' className='hover:underline'>
+                Nitro
+              </Link>
+              <Link to='/discover' className='hover:underline'>
+                Discover
+              </Link>
+              <Link to='/safety' className='hover:underline'>
+                Safety
+              </Link>
+              <Link to='/servers' className='hover:underline'>
+                Support
+              </Link>
+              <Link to='/blog' className='hover:underline'>
+                Blog
+              </Link>
+              <Link to='/careers' className='hover:underline'>
+                Careers
+              </Link>
+            </div>
+            <Link to='/login'>
+              <Button className='text-black bg-white rounded-full hover:bg-white hover:shadow-xl'>
+                Sign up
+              </Button>
+            </Link>
+          </div>
+          <SectionInView className='flex justify-between items-center max-w-6xl gap-10 mt-10'>
+            <div className=''>
+              <h1 className='text-6xl font-bold'>DISCORD SAFETY CENTER</h1>
+              <p className='mt-10'>
+                Discord is the place to hang out with your friends and build
+                community around shared interests. We’re committed to creating a
+                safe, inclusive and welcoming place.
+              </p>
+              <p className='mt-10'>
+                That’s why we’re working to make Discord private and safe, by
+                design. Learn how to shape the best experience for yourself and
+                find the resources you need, whether you’re a teen, parent,
+                educator, or long-time Discord user.
+              </p>
+            </div>
+            <img
+              src={bannerSafety}
+              alt='banner'
+              className='ml-6 h-[30rem] w-[30rem]'
+            />
+          </SectionInView>
+        </div>
+      </header>
+      <div className='flex text-black justify-between items-center max-w-6xl mx-auto gap-10 my-20'>
+        <div className='flex flex-col space-y-10'>
+          <div className='relative flex w-[36rem] overflow-hidden flex-col items-center space-y-6  rounded-lg h-[46rem] bg-[#f6f6f6]'>
+            <h1 className='text-4xl mt-28 font-bold'>iOS</h1>
+            <Button className='bg-black text-white rounded-full text-xl p-8 hover:bg-black hover:shadow-md hover:scale-110'>
+              Download
+            </Button>
+            <img src={ios} alt='ios' className='absolute top-[21.5rem]' />
+          </div>
+          <div className='relative flex w-[36rem] flex-col overflow-hidden items-center space-y-6  rounded-lg h-[40rem] bg-[#f6f6f6]'>
+            <h1 className='text-4xl mt-28 font-bold'>Linux</h1>
+            <DropdownButton></DropdownButton>
+            <img src={linux} alt='ios' className='absolute top-[16rem]' />
+          </div>
+          <div className='relative flex w-[36rem] flex-col overflow-hidden items-center space-y-6 rounded-lg h-96 bg-[#f6f6f6]'>
+            <h1 className='text-4xl mt-28 font-bold'>Feeling experimental?</h1>
+            <p>
+              Try our Public Test Build and test new features before they
+              launch.
+            </p>
+            <DropdownDownloadPublic></DropdownDownloadPublic>
+          </div>
+        </div>
+        <div className='flex flex-col space-y-10'>
+          <div className='relative flex w-[36rem] overflow-hidden flex-col items-center space-y-6  rounded-lg h-[46rem] bg-[#f6f6f6]'>
+            <h1 className='text-4xl mt-14 font-bold'>Android</h1>
+            <Button className='bg-black text-white rounded-full text-xl p-8 hover:bg-black hover:shadow-md hover:scale-110'>
+              Download
+            </Button>
+            <img src={android} alt='ios' className='absolute top-[13.5rem]' />
+          </div>
+          <div className='relative flex w-[36rem] overflow-hidden flex-col items-center space-y-6  rounded-lg h-[38rem] bg-[#f6f6f6]'>
+            <h1 className='text-4xl mt-14 font-bold'>Mac</h1>
+            <Button className='bg-black text-white rounded-full text-xl p-8 hover:bg-black hover:shadow-md hover:scale-110'>
+              Download
+            </Button>
+            <img src={mac} alt='ios' className='absolute top-[13.5rem]' />
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
 }
