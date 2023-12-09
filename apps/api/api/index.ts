820e93fe95dcd24955b38e091d3e8bbdab83fddd
Võ Phước Thạnh <phuocthanh2k03@gmail.com>
2023-12-09 09:11:55 +0700

    feat: update BE

--- apps/api/api/index.ts
+++ apps/api/api/index.ts
@@ -1,6 +1,6 @@
 import { Hono } from 'hono';
 import { handle } from 'hono/vercel';
-import { serve } from '@hono/node-server';
+// import { serve } from '@hono/node-server';
 import { cors } from 'hono/cors';
 import { logger } from 'hono/logger';
 import { router as auth } from '../modules/auth/auth.controller';
@@ -19,11 +19,11 @@ app.use(
 app.route('/', auth);
 app.route('/orgs', orgs);
 
-serve(app, () => {
-  console.log('Server is running on http://localhost:3000');
-});
+// serve(app, () => {
+//   console.log('Server is running on http://localhost:3000');
+// });
 
-// export const config = {
-//   runtime: 'edge',
-// };
-// export default handle(app);
+export const config = {
+  runtime: 'edge',
+};
+export default handle(app);
