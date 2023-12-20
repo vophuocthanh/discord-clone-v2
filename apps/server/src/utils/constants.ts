3f43ea5b90afed599262eab250bcf8ddb2175131
Võ Phước Thạnh <phuocthanh2k03@gmail.com>
2023-12-20 22:38:00 +0700

    feat(BE): fix

--- apps/server/src/utils/constants.ts
+++ apps/server/src/utils/constants.ts
@@ -2,6 +2,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+export const WEB_URL = process.env.WEB_URL;
 export const MAIL_TRANSPORT = process.env.MAIL_TRANSPORT;
 export const MAIL_FROM = process.env.MAIL_FROM;
 export const JWT_SECRET = process.env.JWT_SECRET;
